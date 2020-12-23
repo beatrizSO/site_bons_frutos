d1d90e16f52a1c29fd32833a89ae34120a14bbbd
Jonas <jonasdesouza28@gmail.com>
2020-12-23 00:14:58 -0300

    pagina Contato adicionada

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'application#index'
 
   get '/#application', to: 'application#index'
+  get '/#about_section', to: 'application#homepage', as: :about
+  get '/#services', to: 'application#homepage', as: :services
+  get '/contato', to: 'application#contact', as: :contact
 end
