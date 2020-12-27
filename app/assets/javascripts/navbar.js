322093380b3ce91bf855180bf725880cb828b007
Beatriz <you@example.com>
2020-12-27 16:55:50 -0300

    tentando arrumar o background

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
     const nav_observer = new IntersectionObserver(function (entries) {
         if (entries[0].isIntersecting === true) {
-            document.querySelector("#navbar-2").style.top = "-35vh"
+            document.querySelector("#navbar").style.top = "-35vh"
         } else {
-            document.querySelector("#navbar-2").style.top = "0"
+            document.querySelector("#navbar").style.top = "0"
         }
     }, {threshold: [0.5]});
 
-    nav_observer.observe(document.querySelector("#navbar-1"))
+    nav_observer.observe(document.querySelector("#navbar"))
 });
